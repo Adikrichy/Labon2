6d3467c9e1d0f53dd5907ab6e9c49341d6b18403
Adikrichy <adil.erzhanoc.70@gmail.com>
2024-12-02 10:10:52 +0600

    Initial commit

---
+++ src/main/java/com/example/Projects22/Projects22Application.java
@@ -0,0 +1,13 @@
+package com.example.Projects22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projects22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projects22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projects22/Projects22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projects22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projects22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
